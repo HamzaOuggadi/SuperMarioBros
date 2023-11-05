448b82bf6c963f519689c36c8dd4642a1ceadb53
Hamza Ouggadi <45943796+HamzaOuggadi@users.noreply.github.com>
2023-11-05 15:47:29 +0100

    Fixed Mac OS arm64 issue, app won't start on first thread

--- src/main/java/net/hamzaouggadi/Main.java
+++ src/main/java/net/hamzaouggadi/Main.java
@@ -10,16 +10,5 @@ public class Main {
         Window window = Window.get();
         window.run();
 
-
-/*        long pid = LibC.getpid();
-        System.out.println(System.getenv("JAVA_STARTED_ON_FIRST_THREAD_" + pid));
-        System.out.println(System.getProperty("file.separator"));
-        System.out.println(ProcessHandle.current().info().command().orElseThrow());
-        System.out.println(ManagementFactory.getRuntimeMXBean().getInputArguments());
-        System.out.println(System.getProperty("java.class.path"));
-        System.out.println(System.getenv("JAVA_MAIN_CLASS_" + pid));
-
-        System.out.println(System.getProperty("os.name"));
-        System.out.println(System.getProperty("os.arch"));*/
     }
 }
